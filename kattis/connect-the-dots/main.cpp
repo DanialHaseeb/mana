590abbd29211d42b89aadaab89bdf2fc8a1e0cc5
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2023-08-31 13:55:21 +0500

    Added connect-the-dots

--- kattis/connect-the-dots/main.cpp
+++ kattis/connect-the-dots/main.cpp
@@ -6,10 +6,9 @@
  */
 
 #include <iostream>
-#include <vector>
-#include <string>
-#include <array>
 #include <unordered_map>
+#include <string>
+#include <vector>
 // For std::pair.
 #include <utility>
 // For std::min and std::max.
@@ -54,99 +53,67 @@ auto locateDots(const vector<string>& image)
 	return coordinates;
 }
 
-auto connectHorizontally(vector<string>& image,
-	                       const size_t x1,
-                         const size_t x2,
-                         const size_t y)
+auto connect(vector<string>& image, const array<char, 62>& dots)
 {
-	/// The start of the horizontal line.
-	const auto start{ min(x1, x2) + 1 };
-
-	/// The end of the horizontal line.
-	const auto end{ max(x1, x2) - 1 };
-
-	for (size_t i{ start }; i <= end; ++i)
-	{
-		if (image[y][i] == '|')
-		{ image[y][i] = '+'; }
-		else if (image[y][i] == '.')
-		{ image[y][i] = '-'; }
-	}
-}
-
-auto connectVertically(vector<string>& image,
-                       const size_t y1,
-                       const size_t y2,
-                       const size_t x)
-{
-	/// The start of the horizontal line.
-	const auto start{ min(y1, y2) + 1 };
-
-	/// The end of the horizontal line.
-	const auto end{ max(y1, y2) - 1 };
-
-	for (size_t i{ start }; i <= end; ++i)
-	{
-		if (image[i][x] == '-')
-		{ image[i][x] = '+'; }
-		else if (image[i][x] == '.')
-		{ image[i][x] = '|'; }
-	}
-}
-
-auto connectDots(vector<string>& image,
-                 const pair<size_t, size_t>& position1,
-                 const pair<size_t, size_t>& position2)
-{
-	const auto [y1, x1]{ position1 };
-	const auto [y2, x2]{ position2 };
-
-	// If the dots are in the same row, connect them horizontally.
-	if (y1 == y2)
-	{ connectHorizontally(image, x1, x2, y1); }
-	// If the dots are in the same column, connect them vertically.
-	else
-	{ connectVertically(image, y1, y2, x1); }
-}
-
-/**
- * Connects the dots in an image.
- *
- * @param image The image to be processed.
- */
-auto connect(vector<string>& image, const array<char, 62>& Σ)
-{
-	/// A map of the coordinates of the dots in the image.
 	auto coordinates{ locateDots(image) };
 
 	for (size_t i{}; i < (coordinates.size() - 1); ++i)
 	{
-		/// The first dot to be connected.
-		const auto dot1{ Σ[i] };
+		const auto dot1{ dots[i] };
+		const auto dot2{ dots[i + 1] };
 
-		/// The second dot to be connected.
-		const auto dot2{ Σ[i + 1] };
+		const auto& [y1, x1] = coordinates[dot1];
+		const auto& [y2, x2] = coordinates[dot2];
 
-		/// The coordinates of the first dot.
-		const auto position1{ coordinates[dot1] };
+		if (y1 == y2)
+		{
+			const auto y{ y1 };
+			const auto start{ min(x1, x2) };
+			const auto end{ max(x1, x2) };
 
-		/// The coordinates of the second dot.
-		const auto position2{ coordinates[dot2] };
+			for(auto j{ start }; j < end; ++j)
+			{
+				const auto pixel{ image[y][j] };
 
-		connectDots(image, position1, position2);
+				if (pixel == '.')
+				{ image[y][j] = '-'; }
+				else if (pixel == '|')
+				{ image[y][j] = '+'; }
+			}
+		}
+		else
+		{
+			const auto x{ x1 };
+			const auto start{ min(y1, y2) };
+			const auto end{ max(y1, y2) };
+
+			for(auto j{ start }; j < end; ++j)
+			{
+				const auto pixel{ image[j][x] };
+
+				if (pixel == '.')
+				{ image[j][x] = '|'; }
+				else if (pixel == '-')
+				{ image[j][x] = '+'; }
+			}
+		}
 	}
 }
 
-auto print(const vector<string>& image)
+/**
+ * Prints the image to standard output.
+ *
+ * @param image The image to be printed.
+*/
+auto output(const vector<string>& image)
 {
-	for (const auto& row : image)
+	for(const auto& row : image)
 	{ cout << row << '\n'; }
 }
 
 auto main() -> int
 {
-	/// The characters that represent the dots in an image.
-	const array Σ
+	const array dots
 	{
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
 		'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
@@ -155,38 +122,30 @@ auto main() -> int
 		'Y', 'Z'
 	};
 
-	/// An image to be processed.
 	vector<string> image;
 
-	/// A row of an image.
-	string row{};
+	string row;
 
 	while (getline(cin, row))
 	{
 		if (row.empty())
 		{
-			// The image has been fully read.
-			// The dots inside the image need to be connected now.
-			connect(image, Σ);
+			connect(image, dots);
 
-			print(image);
+			output(image);
 
-			// Reset the image for the next test case.
 			image.clear();
 
-			// Leave a blank line between test cases.
+			// Print a blank line between test cases.
 			cout << '\n';
-
-			continue;
 		}
-
-		image.push_back(row);
+		else
+		{ image.push_back(row); }
 	}
 
-	// Connect the dots for the last image.
-	connect(image, Σ);
+	connect(image, dots);
 
-	print(image);
+	output(image);
 
 	return 0;
 }
