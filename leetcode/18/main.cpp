39afcc84c1a6f5a7d27c366cece908e8ceab51b1
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2023-09-01 10:41:23 +0500

    Fixed indentation

--- leetcode/18/main.cpp
+++ leetcode/18/main.cpp
@@ -5,68 +5,71 @@
 
 using namespace std;
 
-class Solution {
-public:
-	auto fourSum(vector<int>& nums, int target) -> vector<vector<int>> {
-		vector<vector<int>> results;
-
-		if (nums.size() < 4)
-		{ return results; }
-
-		sort(nums.begin(), nums.end());
-
-		const auto n = nums.size();
-
-		for (size_t i{}; i < (n - 3); ++i)
+class Solution
+{
+	public:
+		auto fourSum(vector<int>& nums, int target) -> vector<vector<int>>
 		{
-			// Skip duplicates for i.
-			if ((i > 0) and (nums[i] == nums[i-1]))
-			{ continue; }
+			vector<vector<int>> results;
 
-			for (auto j{ i + 1 }; j < (n - 2); ++j)
+			if (nums.size() < 4)
+			{ return results; }
+
+			sort(nums.begin(), nums.end());
+
+			const auto n = nums.size();
+
+			for (size_t i{}; i < (n - 3); ++i)
 			{
-				// Skip duplicates for j.
-				if ((j > (i + 1)) and (nums[j] == nums[j-1]))
+				// Skip duplicates for i.
+				if ((i > 0) and (nums[i] == nums[i-1]))
 				{ continue; }
 
-				auto start{ j + 1 };
-				auto end{ n - 1 };
-
-				while (start < end)
+				for (auto j{ i + 1 }; j < (n - 2); ++j)
 				{
-					long long a{ nums[i] };
-					long long b{ nums[j] };
-					long long c{ nums[start] };
-					long long d{ nums[end] };
+					// Skip duplicates for j.
+					if ((j > (i + 1)) and (nums[j] == nums[j-1]))
+					{ continue; }
 
-					long long sum{ a + b + c + d };
+					auto start{ j + 1 };
+					auto end{ n - 1 };
 
-					if (sum == target) {
-						results.push_back
-						({
-							static_cast<int>(a),
-							static_cast<int>(b),
-							static_cast<int>(c),
-							static_cast<int>(d)
-						});
+					while (start < end)
+					{
+						long long a{ nums[i] };
+						long long b{ nums[j] };
+						long long c{ nums[start] };
+						long long d{ nums[end] };
 
-						// Skip duplicates for start and end
-						while ((start < end) and (nums[start] == nums[start + 1]))
+						long long sum{ a + b + c + d };
+
+						if (sum == target)
+						{
+							results.push_back
+							({
+								static_cast<int>(a),
+								static_cast<int>(b),
+								static_cast<int>(c),
+								static_cast<int>(d)
+							});
+
+							// Skip duplicates for start and end
+							while ((start < end) and (nums[start] == nums[start + 1]))
+							{ ++start; }
+
+							while ((start < end) and (nums[end] == nums[end - 1]))
+							{ --end; }
+
+							++start;
+							--end;
+						}
+						else if (sum < target)
 						{ ++start; }
-
-						while ((start < end) and (nums[end] == nums[end - 1]))
+						else
 						{ --end; }
-
-						++start;
-						--end;
 					}
-					else if (sum < target)
-					{ ++start; }
-					else
-					{ --end; }
 				}
 			}
+			return results;
 		}
-		return results;
-	}
 };
