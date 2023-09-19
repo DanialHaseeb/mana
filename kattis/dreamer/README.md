e920fa0aa098def2510130f200d314ebf63e35b7
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2023-09-19 20:47:21 +0500

    Added dreamer

--- kattis/dreamer/README.md
+++ kattis/dreamer/README.md
@@ -10,9 +10,57 @@ a valid date.
 
 Since Bash was born on January 1<sup>st</sup>, 2000, he knows that the date was
 on or after January 1<sup>st</sup>, 2000. (Note that the date can be January
-1<sup>st</sup>, 2000---it means Bash is destined to be the best Pokemon trainer
+1<sup>st</sup>, 2000---it means Bash is destined to be the best Pokenom trainer
 since he was born!).
 
 Bash really wants to know the date when he become the best Pokenom trainer.  How
 many possible valid dates could there be?  What is the earliest valid date that
 Bash could become the best Pokenom trainer?
+
+## Notes
+
+On a leap year, February has $29$ days.  Following are the rules for determining
+leap years:
+
+* A year divisible by $400$ is a leap year,
+
+* A year divisible by $100$ but not by $400$ is NOT a leap year,
+
+* A year divisible by $4$ but not by $100$ is a leap year,
+
+* A year not divisible by $4$ is NOT a leap year.
+
+## Input
+
+* The first line contains one integer---the number of test cases.
+
+* Each of the next  lines describes one test case, contains eight digits in the
+  format `XX XX XXXX` (eight digits, separated by two blank spaces).  Note that
+	the input might not represent a valid date.
+
+Note that the first month of the year is represented by $01$, and the first day
+of the month by $01$.
+
+## Output
+
+For each test case, output a single line containing the number of possible dates
+and the earliest date which Bash could become the best Pokenom trainer, in the
+format `DD MM YYYY`.  If there are no valid dates, print a single line
+containing `0` (zero) instead.
+
+### Sample Input 1
+
+```
+3
+04 11 2018
+23 45 6789
+01 01 0002
+```
+
+### Sample Output 1
+
+```
+524 18 11 2004
+0
+4 01 01 2000
+```
---
+++ kattis/dreamer/main.py
@@ -0,0 +1,95 @@
+"""
+Dreamer
+
+:see: https://open.kattis.com/problems/dreamer
+
+:author: Danial Haseeb
+
+"""
+
+import itertools
+import sys
+
+
+def is_leap(year: int):
+	if year % 400 == 0:
+		return True
+
+	if year % 100 == 0:
+		return False
+
+	if year % 4 == 0:
+		return True
+
+	return False
+
+
+class Date:
+	def __init__(self, digits: tuple[str, ...]):
+		tokens = "".join(list(itertools.chain.from_iterable(digits)))
+
+		self.day = int(tokens[0] + tokens[1])
+		self.month = int(tokens[2] + tokens[3])
+		self.year = int(tokens[4] + tokens[5] + tokens[6] + tokens[7])
+
+		self.days_in_months = { 1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11:30, 12: 31 }
+
+		if is_leap(self.year):
+			self.days_in_months[2] = 29
+
+	def is_valid(self):
+		if self.year < 2000:
+			return False
+
+		if (self.month < 1) or (self.month > 12):
+			return False
+
+		if (self.day < 1) or (self.day > self.days_in_months[self.month]):
+			return False
+
+		return True
+
+	def __hash__(self):
+		return hash((self.day, self.month, self.year))
+
+	def __eq__(self, other: object):
+		if not isinstance(other, Date):
+			return False
+
+		return (self.day, self.month, self.year) == (other.day, other.month, other.year)
+
+	def __lt__(self, other: object):
+		if not isinstance(other, Date):
+			return False
+
+		if self.year == other.year:
+			if self.month == other.month:
+				return self.day < other.day
+
+			return self.month < other.month
+
+		return self.year < other.year
+
+	def __repr__(self):
+		return f"{self.day:=02} {self.month:=02} {self.year:=04}"
+
+
+t = int(sys.stdin.readline().rstrip())
+
+for i in range(t):
+	digits = "".join(sys.stdin.readline().split())
+
+	dates: set[Date] = set()
+
+	earliest_date: Date | None = None
+
+	for permutation in itertools.permutations(digits):
+		date = Date(permutation)
+
+		if date.is_valid():
+			dates.add(date)
+
+			if (date < earliest_date) or (earliest_date is None):
+				earliest_date = date
+
+	print(f"{len(dates)} {earliest_date if earliest_date is not None else ''}")
